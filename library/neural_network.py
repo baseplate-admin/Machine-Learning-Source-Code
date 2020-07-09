dc4c10e987dc838c663b225c52fb515d5cc596c8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2020-07-09 22:21:11 +0600

    Update neural_network.py

--- library/neural_network.py
+++ library/neural_network.py
@@ -1,13 +1,9 @@
-import os
 
-ask_base_dir=input("What is your base extracted zip file directory?")
-train_dir=os.path.join(ask_base_dir,"train")
-validation_dir = os.path.join(ask_base_dir,"validation")
-
-ask_directory_folder=input("How many training variable are there?")
 
 import tensorflow as tf
 import tensorflow.keras
+import tensorflow.keras.preprocessing.image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def model(number):
     print("This is Convolutional Neural Network")
@@ -29,3 +25,55 @@ def model(number):
         tf.keras.layers.Dense(512,activation='relu'),
         tf.keras.layers.Dense(1, activation='sigmoid'),
         """)
+        input_shape_ask_height=input("What is your image height?")
+        input_shape_ask_width=input("What is your Image Width?")
+        input_ask_binary_or_categorical=input("""
+        Is your image binary or categorical?
+        If binary Press 1
+        If Categorical Press 2.
+        """)
+        if input_ask_binary_or_categorical==1:
+            activation_dense='sigmoid'
+            crossentropy='binary_crossentropy'
+            class_1='binary'
+        elif input_ask_binary_or_categorical==2:
+            activation_dense='softmax'
+            crossentropy='categorical_crossentropy'
+            class_1='categorical'
+        def models():
+            model = tf.keras.models.Sequential([
+                tf.keras.layers.Conv2D(16,(3,3),activation='relu',input_shape=(input_shape_ask_width,input_shape_ask_height,3)),
+                tf.keras.layers.MaxPool2D(2,2),
+                tf.keras.layers.Conv2D(32,(3,3),activation='relu'),
+                tf.keras.layers.MaxPool2D(2,2),
+                tf.keras.layers.Conv2D(64,(3,3), activation='relu'),
+                tf.keras.layers.MaxPool2D(2,2),
+                tf.keras.layers.Flatten(),
+                tf.keras.layers.Dense(512,activation='relu'),
+                tf.keras.layers.Dense(1, activation=activation_dense)
+            ])
+            model.summary()
+            model.compile(optimizer = 'adam',
+            loss=crossentropy,metrics = ['accuracy'])
+            
+            
+            train_datagen= ImageDataGenerator(rescale=1.0/255.0)
+            validation_datagen= ImageDataGenerator(rescale=1.0/255.0)
+            train_generator=train_datagen.flow_from_directory(
+                train_dir,
+                target_size=(input_shape_ask_width,input_shape_ask_height),
+                class_mode=class_1,
+                batch_size=64
+            )
+            validation_generator=validation_datagen.flow_from_directory(
+                validation_dir,
+                target_size=(input_shape_ask_width,input_shape_ask_height),
+                class_mode=class_1,
+                batch_size=64
+            )
+            history=model.fit(train_generator,
+            validation_data=validation_generator,
+            steps_per_epoch=100,
+            epochs=25,
+            validation_steps=50,
+            verbose=0)
